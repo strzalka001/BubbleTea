879718371cde46a475673c6de035ecc100cfe009
Milek <emilqaa@gmail.com>
2017-01-10 15:13:10 +0100

    lokale

--- app/src/main/java/com/example/milek/projektztp/KoszykActivity.java
+++ app/src/main/java/com/example/milek/projektztp/KoszykActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,11 @@ public class KoszykActivity extends AppCompatActivity {
 
         Listakoszyk = (ListView) findViewById( R.id.lista_koszyk);
         //pole = (TextView) findViewById(R.id.textView4);
+
+        if(TwojKoszyk.pobierzKoszyk().isEmpty()){
+            Toast.makeText(this," Twoj koszyk jest pusty!",Toast.LENGTH_LONG).show();
+        }
+
         Listakoszyk.setAdapter(new ListaAdapter(this, TwojKoszyk.pobierzKoszyk()));
         Listakoszyk.setClickable(true);
 
